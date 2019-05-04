fba0c923faefed7148fbcdce84a0649b84710b00
Jassar Al_Akhars <jalakhras@blessedtreeit.com>
2019-05-05 00:14:32 +0300

    Using Unit test Tools

--- DefensiveCodingSampel/DCS.BL.Test/CustomerTests.cs
+++ DefensiveCodingSampel/DCS.BL.Test/CustomerTests.cs
@@ -3,22 +3,25 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace DCS.BL.Test
 {
-    [TestClass]
+    [TestClass()]
     public class CustomerTests
     {
         [TestMethod]
         public void CalculatePercentOfGoalStepsTestValid()
         {
-            //Arrange
+            //-- Arrange
             var customer = new Customer();
             string goalSteps = "5000";
             string actualSteps = "2000";
             decimal expected = 40M;
+
             //-- Act
             var actual = customer.CalculatePercentOfGoalSteps(goalSteps, actualSteps);
+
             //-- Assert
             Assert.AreEqual(expected, actual);
         }
+
         [TestMethod]
         public void CalculatePercentOfGoalStepsTestValidAndSame()
         {
@@ -51,5 +54,42 @@ namespace DCS.BL.Test
             Assert.AreEqual(expected, actual);
         }
 
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void CalculatePercentOfGoalStepsTestGoalIsNull()
+        {
+            //-- Arrange
+            var customer = new Customer();
+            string goalSteps = null;
+            string actualSteps = "2000";
+
+            //-- Act
+            var actual = customer.CalculatePercentOfGoalSteps(goalSteps, actualSteps);
+
+            //-- Assert
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void CalculatePercentOfGoalStepsTestGoalIsNotNumeric()
+        {
+            //-- Arrange
+            var customer = new Customer();
+            string goalSteps = "one";
+            string actualSteps = "2000";
+
+            //-- Act
+            try
+            {
+                var actual = customer.CalculatePercentOfGoalSteps(goalSteps, actualSteps);
+            }
+            catch (Exception ex)
+            {
+                Assert.AreEqual("Goal must be numeric", ex.Message);
+                throw;
+            }
+
+            //-- Assert
+        }
     }
 }
