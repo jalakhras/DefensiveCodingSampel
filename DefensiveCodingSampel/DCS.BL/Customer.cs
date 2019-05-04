62f1901421c717bf68097fe319b4b13b9fa9a10c
Jassar Al_Akhars <jalakhras@blessedtreeit.com>
2019-05-04 22:05:58 +0300

    Validating Method Parameters

--- DefensiveCodingSampel/DCS.BL/Customer.cs
+++ DefensiveCodingSampel/DCS.BL/Customer.cs
@@ -17,5 +17,26 @@ namespace DCS.BL
             // If not,
             // request an email address from the user.
         }
+
+        public decimal CalculatePercentOfGoalSteps(string goalSteps, string actualSteps)
+        {
+            // Try 3
+            decimal goalStepCount = 0;
+            decimal actualStepCount = 0;
+
+            if (string.IsNullOrWhiteSpace(goalSteps)) throw new ArgumentException("Goal must be entered", "goalSteps");
+            if (string.IsNullOrWhiteSpace(actualSteps)) throw new ArgumentException("Actual steps count must be entered", "actualSteps");
+
+            if (!decimal.TryParse(goalSteps, out goalStepCount)) throw new ArgumentException("Goal must be numeric", "goalSteps");
+            if (!decimal.TryParse(actualSteps, out actualStepCount)) throw new ArgumentException("Actual steps must be numeric", "actualSteps");
+
+            return CalculatePercentOfGoalSteps(goalStepCount, actualStepCount);
+        }
+
+        public decimal CalculatePercentOfGoalSteps(decimal goalStepCount, decimal actualStepCount)
+        {
+            if (goalStepCount <= 0) throw new ArgumentException("Goal must be greater than 0", "goalSteps");
+            return (actualStepCount / goalStepCount) * 100;
+        }
     }
 }
---
+++ DefensiveCodingSampel/DefensiveCodingSampel/PedometerWin.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace DefensiveCodingSampel
+{
+    partial class PedometerWin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.GoalTextBox = new System.Windows.Forms.TextBox();
+            this.StepsTextBox = new System.Windows.Forms.TextBox();
+            this.Calculate_Button = new System.Windows.Forms.Button();
+            this.ResultLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(80, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(147, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Step Golas For Today";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(80, 144);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(156, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Number of steps Today";
+            // 
+            // GoalTextBox
+            // 
+            this.GoalTextBox.Location = new System.Drawing.Point(286, 43);
+            this.GoalTextBox.Name = "GoalTextBox";
+            this.GoalTextBox.Size = new System.Drawing.Size(191, 22);
+            this.GoalTextBox.TabIndex = 2;
+            // 
+            // StepsTextBox
+            // 
+            this.StepsTextBox.Location = new System.Drawing.Point(286, 144);
+            this.StepsTextBox.Name = "StepsTextBox";
+            this.StepsTextBox.Size = new System.Drawing.Size(179, 22);
+            this.StepsTextBox.TabIndex = 3;
+            // 
+            // Calculate_Button
+            // 
+            this.Calculate_Button.Location = new System.Drawing.Point(504, 43);
+            this.Calculate_Button.Name = "Calculate_Button";
+            this.Calculate_Button.Size = new System.Drawing.Size(193, 123);
+            this.Calculate_Button.TabIndex = 4;
+            this.Calculate_Button.Text = "Calculate";
+            this.Calculate_Button.UseVisualStyleBackColor = true;
+            this.Calculate_Button.Click += new System.EventHandler(this.Calculate_Button_Click);
+            // 
+            // ResultLabel
+            // 
+            this.ResultLabel.AutoSize = true;
+            this.ResultLabel.Location = new System.Drawing.Point(387, 237);
+            this.ResultLabel.Name = "ResultLabel";
+            this.ResultLabel.Size = new System.Drawing.Size(0, 17);
+            this.ResultLabel.TabIndex = 5;
+            // 
+            // PedometerWin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.ResultLabel);
+            this.Controls.Add(this.Calculate_Button);
+            this.Controls.Add(this.StepsTextBox);
+            this.Controls.Add(this.GoalTextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "PedometerWin";
+            this.Text = "PedometerWin";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox GoalTextBox;
+        private System.Windows.Forms.TextBox StepsTextBox;
+        private System.Windows.Forms.Button Calculate_Button;
+        private System.Windows.Forms.Label ResultLabel;
+    }
+}
