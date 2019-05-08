b89f5a39e3f89f4f169b6ab19aeb40d0264fbb67
Jassar Al_Akhars <jalakhras@blessedtreeit.com>
2019-05-09 00:49:41 +0300

    Using Enum to avoid magic number in a switch statement

--- DefensiveCodingSampel/DCS.BL/Payment.cs
+++ DefensiveCodingSampel/DCS.BL/Payment.cs
@@ -1,20 +1,44 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
+using System.ComponentModel;
 
 namespace DCS.BL
 {
-   public class Payment
+    public enum PaymentType
     {
-  
+        CreditCard = 1,
+        PayPal = 2
+    }
+
+    /// <summary>
+    /// Manages a payment.
+    /// </summary>
+    public class Payment
+    {
+        public int PaymentType { get; set; }
+
         public void ProcessPayment()
         {
-            // -- Process the payment --
-            // If credit card, 
-            // process the credit card payment.
-            // If PayPal, 
-            // process the PayPal payment.
-            // If there is a payment problem, notify the user.
+            PaymentType paymentTypeOption;
+            if (!Enum.TryParse(PaymentType.ToString(), out paymentTypeOption))
+            {
+                throw new InvalidEnumArgumentException("Payment type", (int)PaymentType, typeof(PaymentType));
+            }
+
+            switch (paymentTypeOption)
+            {
+
+                case BL.PaymentType.CreditCard:
+                    // Process credit card
+                    break;
+
+                case BL.PaymentType.PayPal:
+                    // Process PayPal
+                    break;
+
+                default:
+                    throw new ArgumentException();
+            }
+
             // Open a connection
             // Set stored procedure parameters with the payment data.
             // Call the save stored procedure.
